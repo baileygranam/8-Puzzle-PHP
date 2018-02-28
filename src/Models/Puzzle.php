f24c92aca185eee0523c9c3b1b79892898f5dfd7
Bailey Granam <bgranam@stetson.edu>
2018-02-28 02:30:38 -0500

    Added index page for the puzzle. Created the Puzzle Model Class.

---
+++ src/Models/Puzzle.php
@@ -0,0 +1,21 @@
+<?php
+
+class Puzzle
+{
+	private $puzzle;
+	private $dialog;
+	private $emptyX;
+	private $emptyY;
+	private $nums = [1,2,3,4,5,6,7,8,""];
+
+	/* Constructor */
+    public function __construct()
+    {
+        $dialog = "Select a piece to swap.";
+    }
+
+    
+}
+
+
+
